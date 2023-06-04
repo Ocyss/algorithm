abd2bfb9394fe2037fada12850475a0ec7a309b3
Ocyss <qiudie@88.com>
2023-06-04 23:05:28 +0800

    846C

---
+++ CodeForces/800-899/846C/846C_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/EndlessCheng/codeforces-go/main/testutil"
+	"testing"
+)
+
+// TestCF846C https://codeforces.com/problemset/problem/846/C
+// https://codeforces.com/problemset/status/846/problem/C
+// https://github.com/Ocyss
+func TestCF846C(t *testing.T) {
+	// just copy from website
+	rawText := `
+input
+3
+-1 2 3
+output
+0 1 3
+input
+4
+0 0 -1 0
+output
+0 0 0
+input
+1
+10000
+output
+1 1 1`
+	testutil.AssertEqualCase(t, rawText, 0, CF846C)
+}
